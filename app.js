1354212ced3f2bd512bc762eff332e2a7b943e19
ChloeArd <chloe.ardoise@gmail.com>
2020-12-21 15:59:02 +0100

    Exercice 98

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let decompte = 0;
 
 let timeoutExo = function (){
-    setTimeout(function () {
-        if (decompte <= 600000){
+    let timeoutID = setTimeout(function () {
+        if (decompte <= (600000 / 2)){
             let div2 = document.createElement("div");
             div2.innerHTML = "Bonjour, " + decompte;
             document.getElementById("timeout-div").appendChild(div2);
             decompte ++;
             timeoutExo();
+        }
+        else {
+            clearTimeout(timeoutID);
         }
     }, 2000);
 }
